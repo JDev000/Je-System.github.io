cd24b35b2555f1b0a279f076adab70a36b2740ad
JDev000 <124886417+JDev000@users.noreply.github.com>
2023-08-02 19:40:16 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const I_P = document.getElementById('incorrect-pass');
+const body = document.body;
+
+const form_C = document.querySelector('.form-container');
+const logBTN = document.getElementById('log-btn')
+const pass = document.getElementById('password');
+const displayNone = document.getElementById('display-none');
+
+
+const incorrectPassword = document.getElementById('incorrect-pass');
+
+
+function loadHome() {
+    window.location.href = 'http://127.0.0.1:5500/pages/home.html';
+}
+
+
+logBTN.addEventListener('click', loadNow => {
+
+    if (pass.value == 'admin') {
+        loadHome()
+        pass.value = ''
+    } else {
+        displayNone.setAttribute('id', 'incorrect-pass');
+
+        setTimeout(() => {
+            displayNone.remove(); // Remove the entire div element
+            location.reload(); // Refresh the page
+        }, 2500)
+    }
+
+})
+
+
+
+
